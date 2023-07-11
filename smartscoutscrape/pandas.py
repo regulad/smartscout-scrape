3ee7c6518f11ad2bf0da109d9760c30788f9e83b
Parker Wahle <regulad@regulad.xyz>
2023-07-11 13:15:12 -0400

    cleanup work

--- smartscoutscrape/pandas.py
+++ smartscoutscrape/pandas.py
@@ -1,5 +1,20 @@
 """
-A set of utilities for working with smartscout data in pandas.
+A simple array of utilities for moving data from SmartScout to pandas using SQLite.
+
+Copyright 2023 Parker Wahle <regulad@regulad.xyz>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing
+permissions and limitations under the License.
+
 """
 import sqlite3
 import tempfile
